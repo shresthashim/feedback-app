fe284a1bb2788c437ef11d921dfa6d5d9cb02a88
AsHim Shrestha <ashimsth89@gmail.com>
2024-10-25 22:00:18 +0545

    create middleware and signin page

---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
+export { default } from "next-auth/middleware";
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req });
+
+  const url = req.nextUrl;
+
+  if (
+    token &&
+    (url.pathname.startsWith("/sign-in") ||
+      url.pathname.startsWith("/sign-up") ||
+      url.pathname.startsWith("/verify") ||
+      url.pathname.startsWith("/"))
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+
+  if (!token && url.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+}
+
+export const config = {
+  matcher: ["/sign-in", "/sign-up", "/", "/dashboard/:path*", "/verify/:path*"],
+};
