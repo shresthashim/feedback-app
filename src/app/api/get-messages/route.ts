dc4419ef02ccac67c5f56047315d3d0749cd53b1
AsHim Shrestha <ashimsth89@gmail.com>
2024-10-26 17:42:24 +0545

    feat : make get messages api route with aggregration pipeline

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,76 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import UserModel from "@/model/users.model";
+import dbConnect from "@/lib/db";
+import { User } from "next-auth";
+import { NextResponse } from "next/server";
+import mongoose from "mongoose";
+
+export async function GET(req: Request) {
+  await dbConnect();
+
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user;
+
+  if (!session || !session.user) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Unauthorized",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+  const userId = new mongoose.Types.ObjectId(user._id);
+  try {
+    const user = await UserModel.aggregate([
+      {
+        $match: {
+          id: userId,
+        },
+      },
+
+      {
+        $unwind: `$messages`,
+      },
+      {
+        $sort: {
+          "messages.createdAt": -1,
+        },
+      },
+      {
+        $group: {
+          _id: "$_id",
+          messages: {
+            $push: "$messages",
+          },
+        },
+      },
+    ]);
+
+    if (!user) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "No messages found",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Messages found",
+        messages: user[0].messages,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {}
+}
