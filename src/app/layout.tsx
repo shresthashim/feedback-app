5ec0b97a9eb3879d0c3dbf0ac345ad9f1ad2cbed
AsHim Shrestha <ashimsth89@gmail.com>
2024-10-29 21:36:39 +0545

    refactor: add metadata in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,12 @@ import "./globals.css";
 import AuthProvider from "@/context/AuthProvider";
 import { Toaster } from "@/components/ui/toaster";
 import Navbar from "@/components/Navbar";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Feedback App",
+  description: "Anonymous feedback app for everyone to share their thoughts.",
+};
 
 export default function RootLayout({
   children,
