2687d73a30d6788f970012a56d3a274d2e81c072
AsHim Shrestha <ashimsth89@gmail.com>
2024-11-08 17:51:50 +0545

    bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import Autoplay from "embla-carousel-autoplay";
 import { Mail } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import { messages } from "@/data/messages";
+import Autoplay from "embla-carousel-autoplay";
 
 const HomePage = () => {
+  const autoplayPlugin = Autoplay({ delay: 5000, stopOnInteraction: true });
   return (
     <>
       <main className='flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12 bg-gray-800 text-white'>
@@ -15,7 +16,7 @@ const HomePage = () => {
           <p className='mt-3 md:mt-4 text-base md:text-lg'>True Feedback - Where your identity remains a secret.</p>
         </section>
 
-        <Carousel plugins={[Autoplay({ delay: 5000, stopOnInteraction: true })]} className='w-full max-w-lg md:max-w-xl'>
+        <Carousel plugins={[autoplayPlugin]} className='w-full max-w-lg md:max-w-xl'>
           <CarouselContent>
             {messages.map((message, index) => (
               <CarouselItem key={index} className='p-4'>
